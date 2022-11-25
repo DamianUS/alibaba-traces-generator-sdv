7d5480ba1d13a56f4581fbcb7b876c0950f1ac09
alejandrofdez-us <afdez@us.es>
2022-11-25 07:44:14 +0100

    Updated experiment_command_generator.py for google2019

--- google2019/experiment_command_generator.py
+++ google2019/experiment_command_generator.py
@@ -3,26 +3,27 @@ import os
 import stat
 from datetime import datetime
 
+def generate_bash_file(commands, sh_name, trace_name):
+    sh_filename = f'experiments_{trace_name}_{sh_name}_{datetime.now().strftime("%j-%Y-%H-%M")}.sh'
+    sh_file = open(sh_filename, "w")
+    sh_file.writelines(commands)
+    st = os.stat(sh_filename)
+    os.chmod(sh_filename, st.st_mode | stat.S_IEXEC)
 
-def generate_experiment_command(ori_data_filename, trace, sample_size, n_samples, seq_len, epochs, ):
-    return f'python train_sdv.py --ori_data_filename {ori_data_filename} --trace {trace} --sample_size {sample_size} --n_samples {n_samples} --seq_len {seq_len} --epochs {epochs}\n'
+def generate_experiment_command(trace, sample_size, n_samples, seq_len, epochs, experiment_save_dir):
+   return f'python train_sdv.py --trace {trace} --sample_size {sample_size} --n_samples {n_samples} --seq_len {seq_len} --epochs {epochs} --experiment_save_dir {experiment_save_dir}\n'
 
-
-ori_data_filename = ['google2019/data/instance_usage_5min_sample_month.csv']
-trace = ['google2019']
 sample_size = [1]
-n_samples = [0]
-seq_len = [1]
-epochs = [10, 50, 100, 200, 500, 1000, 2000]
+n_samples = [10]
+seq_len = [288]
+epochs = [10, 100, 500, 1000, 1500, 2000]
+parameterization = [sample_size, n_samples, seq_len, epochs]
 
-parameterization = [ori_data_filename, trace, sample_size, n_samples, seq_len, epochs]
+experiment_save_dir = '~/experiments/sdv/google2019/'
+trace = 'google2019'
 
-commands = [generate_experiment_command(ori_data_filename, trace, sample_size, n_samples, seq_len, epochs) for
-            ori_data_filename, trace, sample_size, n_samples, seq_len, epochs in
+commands = [generate_experiment_command(trace, sample_size, n_samples, seq_len, epochs, experiment_save_dir) for
+            sample_size, n_samples, seq_len, epochs in
             list(itertools.product(*parameterization))]
 
-sh_filename = "experiments" + '-' + datetime.now().strftime("%j-%Y-%H-%M") + ".sh"
-sh_file = open(sh_filename, "w")
-sh_file.writelines(commands)
-st = os.stat(sh_filename)
-os.chmod(sh_filename, st.st_mode | stat.S_IEXEC)
+generate_bash_file(commands,'', trace)
